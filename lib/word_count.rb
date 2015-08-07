311d91901051ab831259503a3515c214a79a3236
Lisa Folliard <lisaf37@gmail.com>
2015-08-07 13:58:24 -0700

    Redo first spec to pass

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,13 @@
 class String
-  define_method(:word_count) do
-    self.to_i().+1
+  define_method(:word_count) do |word|
+    counter = 0
+
+    sentence = self.split
+    sentence.each() do |i|
+      if i == word
+        counter = counter.+1
+      end
+    end
+    return counter
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -4,6 +4,10 @@ require('pry')
 
 describe('String#word_count') do
   it('takes a single word string and counts number of instances of the word') do
-    expect(("blue").word_count()).to(eq(1))
+    expect(("blue").word_count("blue")).to(eq(1))
   end
+
+  # it('takes a two word string and counts number of instances of a particular word') do
+  #   expect(("blue blue").word_count()).to(eq(2))
+  # end
 end
